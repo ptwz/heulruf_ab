346fbae238a4ad695cba17f6fe89dd54cae1102c
Peter Turczak <peter@turczak.de>
2016-01-21 11:39:17 +0100

    At least it seems to load something

--- html2/js/heulruf.js
+++ html2/js/heulruf.js
@@ -6,17 +6,34 @@ $.get( "data/", function( data ) {
   // .mp3, parse these URLs and make them into a playlist
   // by splitting them into the name, date, etc part.
   var links = $("a[href$='mp3']", html);
-  var tracklist = {};
+  var tracklist = [];
 
   for (i=0; i<links.length; i++){
   	var loc = links[i].getAttribute("href");
 	var parts = loc.split(/[-.]/);
 	if (parts.length == 3) {
 		var timestamp;
+		var playlist_element = {};
 		timestamp = Date(parseInt(parts[1]))
-		
+		playlist_element['title'] = timestamp;
+		playlist_element['mp3'] = loc;
+		tracklist.push(playlist_element);
 	}
   }
+
+  var myPlaylist = new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_1",
+		cssSelectorAncestor: "#jp_container_1"
+	}, tracklist
+	, {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
   alert( "Load was performed." );
 });
 }
